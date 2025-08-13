258707964ff8c1ea845541a0ae4e3d7adb4738a1
SantosG4EDU <g.a.santos@g4educacao.com>
2025-08-13 14:37:10 -0300

    feat: add RoomModule and TestController on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomModule } from './room/room.module';
+import { TestController } from './test/test.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RoomModule],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
