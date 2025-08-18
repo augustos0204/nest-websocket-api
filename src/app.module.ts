507d47240341b2eeb852a8b144868e12df6ea646
Augustos0204 <course.gabrielaug@gmail.com>
2025-08-18 04:07:18 -0300

    feat: Importing new modules on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 import { RoomModule } from './room/room.module';
-import { TestController } from './test/test.controller';
+import { MetricsModule } from './metrics/metrics.module';
+import { HealthModule } from './health/health.module';
+import { ViewsModule } from './views/views.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    RoomModule
+    EventsModule,
+    RoomModule,
+    MetricsModule,
+    HealthModule,
+    ViewsModule,
   ],
-  controllers: [AppController, TestController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
